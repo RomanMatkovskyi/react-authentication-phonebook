f1d0a4f2b5edb3b5a0e5384e24ea3f6c5725f8cb
RomanMatkovskyi <roman.matkovskyii@gmail.com>
2023-12-17 22:36:50 +0100

    fix import bug(Filter components bug)

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,8 +2,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/contacts/operations';
 import { getIsLoading } from 'redux/contacts/selectors';
-import ContactsList from 'components/contacts/ContactsList';
 import Filter from 'components/Filter/Filter';
+import ContactsList from 'components/contacts/ContactsList';
 import Form from 'components/Form/Form';
 const Contacts = () => {
   const dispatch = useDispatch();
